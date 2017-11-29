33247ca68e5316866e3f56b565dc6e26e0ba0348
EuwewptcML <appTest4@wavemaker.com>
2017-11-29 22:56:00 +0000

    Auto commit Files modified - [securityService,profiles,webapp,project-security.xml]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -6,7 +6,7 @@ var _WM_APP_PROPERTIES = {
   "homePage" : "Main",
   "name" : "Auto_NomsKBWbCW",
   "platformType" : "WEB",
-  "securityEnabled" : "false",
+  "securityEnabled" : "true",
   "supportedLanguages" : "en",
   "timeFormat" : "",
   "type" : "APPLICATION",
