781965643fa8c59ac2ae401fc89f3ff5fd24ebfc
EuwewptcML <appTest4@wavemaker.com>
2017-11-29 22:53:31 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_NomsKBWbCW",
+  "homePage" : "Main",
+  "name" : "Auto_NomsKBWbCW",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
